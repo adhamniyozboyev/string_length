2d8396cced1ba8207efd55e1db80e86501553c48
adhamniyozboyev <adhamniyozboyev77@gmail.com>
2022-07-05 07:26:13 +0500

    string09

--- bin/string_length09.dart
+++ bin/string_length09.dart
@@ -7,6 +7,15 @@
     Returns:
         str: answer
      */
-void main() {
+    String func(String s1, String s2){
+      String o;
+      int a=s1.length;
+      int k=s2.length;
+      if (a>k){
+        return s1+s2;
+      }return s1;
+    }
+    
+void main() {print(func('Destents','Serjant'));
   // write your code here
 }
